dae31526dcfc3fcebf9743f6c589a9552183f2ec
miguelvacuii <contacto@miguelmartinezmadrid.com>
2023-10-28 19:15:59 +0200

    REFACTOR - Extract method y naming
    Se separa el código extraido en funciones aislando la responsabilidad de partes del código y tratando de describir qué hace
    También se renombran algunas variables para que tengan un nombre más explícito

--- ExcelImport/Controllers/UserController.cs
+++ ExcelImport/Controllers/UserController.cs
@@ -19,7 +19,9 @@ namespace ExcelImport.Controllers
 {
     public class UserController : Controller
     {
-        
+
+        private test2Entities repository = new test2Entities();
+
         public ActionResult Index()
         {
             return View();
@@ -32,96 +34,114 @@ namespace ExcelImport.Controllers
         }
 
         [HttpPost]
-        public JsonResult UploadExcel(User users, HttpPostedFileBase FileUpload)
+        public JsonResult UploadExcel(User users, HttpPostedFileBase fileUpload)
         {
 
-            List<string> data = new List<string>();
+            List<string> errorMessage = new List<string>();
 
-            if (FileUpload == null) {
-                data.Add("<ul>");
-                if (FileUpload == null) data.Add("<li>Please choose Excel file</li>");
-                data.Add("</ul>");
-                data.ToArray();
-                return Json(data, JsonRequestBehavior.AllowGet);
+            if (fileUpload == null) {
+                errorMessage.Add("<ul>");
+                if (fileUpload == null) errorMessage.Add("<li>Please choose Excel file</li>");
+                errorMessage.Add("</ul>");
+                errorMessage.ToArray();
+                return Json(errorMessage, JsonRequestBehavior.AllowGet);
             }
 
-            if (FileUpload.ContentType != "application/vnd.ms-excel" || FileUpload.ContentType != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
-                data.Add("<ul>");
-                data.Add("<li>Only Excel file format is allowed</li>");
-                data.Add("</ul>");
-                data.ToArray();
-                return Json(data, JsonRequestBehavior.AllowGet);
+            if (fileUpload.ContentType != "application/vnd.ms-excel" || fileUpload.ContentType != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
+                errorMessage.Add("<ul>");
+                errorMessage.Add("<li>Only Excel file format is allowed</li>");
+                errorMessage.Add("</ul>");
+                errorMessage.ToArray();
+                return Json(errorMessage, JsonRequestBehavior.AllowGet);
             }
 
-            UploadExcelUseCase(users, FileUpload);
-
-            return Json("success", JsonRequestBehavior.AllowGet);
-            
+            return UploadExcelUseCase(users, fileUpload);
         }
 
-        public void UploadExcelUseCase(User users, HttpPostedFileBase FileUpload) {
+        public JsonResult UploadExcelUseCase(User users, HttpPostedFileBase fileUpload) {
 
-            string filename = FileUpload.FileName;
-            string targetpath = Server.MapPath("~/Doc/");
-            FileUpload.SaveAs(targetpath + filename);
-            string pathToExcelFile = targetpath + filename;
-            var connectionString = "";
+            string fileName = fileUpload.FileName;
+            string targetPath = Server.MapPath("~/Doc/");
+            fileUpload.SaveAs(targetPath + fileName);
+            string pathToExcelFile = targetPath + fileName;
 
-            if (filename.EndsWith(".xls")) {
-                connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", pathToExcelFile);
+            CreateConnection(fileName, pathToExcelFile);
+
+            List<User> userList = GetUsersFromExcel(pathToExcelFile);
+
+            List<string> errorMessage = SaveUsers(userList);
+
+            if (errorMessage.Any()) {
+                return Json(errorMessage, JsonRequestBehavior.AllowGet);
             }
-            else if (filename.EndsWith(".xlsx"))
-            {
+
+            DeleteFile(pathToExcelFile);
+            return Json("success", JsonRequestBehavior.AllowGet);
+        }
+
+        private void CreateConnection(string fileName, string pathToExcelFile) {
+            string connectionString = "";
+            if (fileName.EndsWith(".xls")) {
+                connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", pathToExcelFile);
+            } else if (fileName.EndsWith(".xlsx")) {
                 connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES;IMEX=1\";", pathToExcelFile);
             }
 
-            var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString);
-            var ds = new DataSet();
-            adapter.Fill(ds, "ExcelTable");
-            DataTable dtable = ds.Tables["ExcelTable"];
+            if (connectionString != "") {
+                var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString);
+                var dataSet = new DataSet();
+                adapter.Fill(dataSet, "ExcelTable");
+                DataTable dtable = dataSet.Tables["ExcelTable"];
+            }
+        }
+
+        private List<User> GetUsersFromExcel(string pathToExcelFile) {
             string sheetName = "Sheet1";
             var excelFile = new ExcelQueryFactory(pathToExcelFile);
-            var artistAlbums = from a in excelFile.Worksheet<User>(sheetName) select a;
-            foreach (var a in artistAlbums)
-            {
-                try
-                {
-                    if (a.Name != "" && a.Address != "" && a.ContactNo != "")
-                    {
-                        User TU = new User();
-                        TU.Name = a.Name;
-                        TU.Address = a.Address;
-                        TU.ContactNo = a.ContactNo;
-                        db.Users.Add(TU);
-                        db.SaveChanges();
+
+            return (from rows in excelFile.Worksheet<User>(sheetName) select rows) as List<User>;
+        }
+
+        private List<string> SaveUsers(List<User> userList) {
+            List<string> errorMessage = new List<string>();
+
+            foreach (User user in userList) {
+                try {
+                    if (user.Name != "" && user.Address != "" && user.ContactNo != "") {
+                        User userUpdated = new User();
+                        userUpdated.Name = user.Name;
+                        userUpdated.Address = user.Address;
+                        userUpdated.ContactNo = user.ContactNo;
+                        repository.Users.Add(userUpdated);
+                        repository.SaveChanges();
+                    } else {
+                        errorMessage.Add("<ul>");
+                        if (user.Name == "" || user.Name == null) errorMessage.Add("<li> name is required</li>");
+                        if (user.Address == "" || user.Address == null) errorMessage.Add("<li> Address is required</li>");
+                        if (user.ContactNo == "" || user.ContactNo == null) errorMessage.Add("<li>ContactNo is required</li>");
+                        errorMessage.Add("</ul>");
+                        errorMessage.ToArray();
+                        
                     }
-                    else
-                    {
-                        data.Add("<ul>");
-                        if (a.Name == "" || a.Name == null) data.Add("<li> name is required</li>");
-                        if (a.Address == "" || a.Address == null) data.Add("<li> Address is required</li>");
-                        if (a.ContactNo == "" || a.ContactNo == null) data.Add("<li>ContactNo is required</li>");
-                        data.Add("</ul>");
-                        data.ToArray();
-                        return Json(data, JsonRequestBehavior.AllowGet);
-                    }
-                }
-                catch (DbEntityValidationException ex)
-                {
-                    foreach (var entityValidationErrors in ex.EntityValidationErrors)
-                    {
-                        foreach (var validationError in entityValidationErrors.ValidationErrors)
-                        {
+                } catch (DbEntityValidationException ex) {
+                    foreach (var entityValidationErrors in ex.EntityValidationErrors) {
+                        foreach (var validationError in entityValidationErrors.ValidationErrors) {
                             Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
                         }
                     }
                 }
             }
 
+            return Json(errorMessage, JsonRequestBehavior.AllowGet);
+        }
+
+        private void DeleteFile(String pathToExcelFile) {
             if ((System.IO.File.Exists(pathToExcelFile)))
             {
                 System.IO.File.Delete(pathToExcelFile);
             }
         }
+
     }
+
 }
